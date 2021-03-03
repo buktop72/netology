03f26b4c05b932100ef99be6563a2de8aa214e14
ViktorSh <jasonx2007@yandex,ru>
2021-03-03 23:09:28 +0400

    исправление stackowerlow.py

--- requests/stackowerlow.py
+++ requests/stackowerlow.py
@@ -1,8 +1,7 @@
 import requests
 from pprint import pprint
 
-def python_tags(url):
-    # url = 'https://api.stackexchange.com/2.2/questions'
+def python_tags(url):    
     params = {'fromdate':'1614470400', 'todate':'1614729600', 'order':'desc', 'sort':'activity', 'tagged':'python',
              'site':'stackoverflow.com'}
     resp = requests.get(url, params = params)
