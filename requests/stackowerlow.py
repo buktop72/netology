f3d210075b56f9a7ea1d31090e3f227870eae182
ViktorSh <jasonx2007@yandex,ru>
2021-03-03 23:04:08 +0400

    добавил задачу 3

---
+++ requests/stackowerlow.py
@@ -0,0 +1,13 @@
+import requests
+from pprint import pprint
+
+def python_tags(url):
+    # url = 'https://api.stackexchange.com/2.2/questions'
+    params = {'fromdate':'1614470400', 'todate':'1614729600', 'order':'desc', 'sort':'activity', 'tagged':'python',
+             'site':'stackoverflow.com'}
+    resp = requests.get(url, params = params)
+    dict = (resp.json())
+    for i in dict['items']:
+        print(i['title'])
+
+python_tags('https://api.stackexchange.com/2.2/questions')
