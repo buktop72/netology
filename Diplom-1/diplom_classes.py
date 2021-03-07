b8c16cfe5604bedb7c6e09c23e81a56b714f6175
ViktorSh <jasonx2007@yandex,ru>
2021-03-07 21:23:54 +0400

    диплом на классах добавил обработку ошибки "каталог уже существует"

--- Diplom-1/diplom_classes.py
+++ Diplom-1/diplom_classes.py
@@ -1,7 +1,6 @@
 import requests
 from pprint import pprint
 
-
 class VkUser:
     url = 'https://api.vk.com/method/photos.get'
     def __init__(self, id):
@@ -27,51 +26,67 @@ class VkUser:
         info = requests.get(url, params=params)
         return str(info.json()['response'][0]['first_name'] + info.json()['response'][0]['last_name'])
 
-
     def get_url_photos(self):
         res = requests.get(self.url, params=self.params)
         result = res.json()
-        # pprint(result)
         img_url_list = []
         img_url_dict = {}
+        x = 2
         for i in result['response']['items']:
             size = 0
+            # pprint(i)
             for j in i['sizes']:
+                # pprint(j)
                 if j['height'] > size:
                     size = j['height']
                     img_url = j['url']
             img_url_list.append(img_url)
-        return img_url_list
+            key = str(i["likes"]['count'])
+            if key not in img_url_dict.keys():
+                img_url_dict[key] = img_url
+            else:
+                key = key + '-' + str(x)
+                img_url_dict[key] = img_url
+                x += 1
+        return img_url_dict
 
-    def up_photos(self, ls, dir):
-        #Создание папки
+    def list_dir(self):
         url = 'https://cloud-api.yandex.net/v1/disk/resources'
-        params = {"path": dir}
+        params = {'path' : '/'}
         with open('ya_token.txt') as f:
             self.token = f.read().strip()
         headers = {"Authorization": self.token}
-        resp = requests.put(url, params=params, headers=headers)
-        #print(resp.status_code)
-        #Загрузка файлов
+        resp = requests.get(url, params=params, headers=headers)
+        ls_dir = []
+        for i in resp.json()['_embedded']['items']:
+            ls_dir.append(i['name'])
+        return ls_dir
+
+    def up_photos(self, dict, dir, list_dir):
+        if dir not in ls_dir:
+            url = 'https://cloud-api.yandex.net/v1/disk/resources'
+            params = {"path": dir}
+            with open('ya_token.txt') as f:
+                self.token = f.read().strip()
+            headers = {"Authorization": self.token}
+            resp = requests.put(url, params=params, headers=headers)
+        else:
+            print(f"Каталог {dir} уже существует!")
+            exit()
         url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
-        for i, j in enumerate(ls):
-            path = dir+ '/'+dir + '-' + str(i)
+        for i in dict:
+            path = dir+ '/'+dir + '_' + str(i)
             params = {"path": path}
             headers = {"Authorization": self.token}
             resp = requests.get(url, params=params, headers=headers)
             print(resp.status_code)
-            # print(resp.json())
-
-
             url_up = resp.json()['href']
-            image = requests.get(j)
+            image = requests.get(dict[i])
             up_file = requests.put(url_up, image.content)
             print(up_file.status_code)
 
-
 vk_client = VkUser('552934290')
 dir_name = vk_client.user_info()
-#pprint(dir_name)
-img_list = vk_client.get_url_photos()
-#pprint(img_list)
-vk_client.up_photos(img_list, dir_name)
+img_dict = vk_client.get_url_photos()
+ls_dir = vk_client.list_dir()
+vk_client.up_photos(img_dict, dir_name, ls_dir)
