a8412a370427e7bedd57b35cc923a51e5cfa3e68
ViktorSh <jasonx2007@yandex,ru>
2021-03-11 16:26:53 +0400

    Исправил логирование и вывод в консоль

--- Diplom-1/diplom.py
+++ Diplom-1/diplom.py
@@ -4,7 +4,7 @@ import json
 from pprint import pprint
 
 logging.basicConfig(
-    level=logging.DEBUG,
+    level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s",
     filename='debug.log',
     filemode='w'
@@ -29,7 +29,6 @@ class VkUser:
         }
     # Получаем информацию о юзере (строка "Имя_Фамилия"):
     def user_info(self):
-        logging.debug('Start user_info')
         url = 'https://api.vk.com/method/users.get'
         params = {
             'user_id': self.id,
@@ -37,57 +36,57 @@ class VkUser:
             'v': '5.130'
         }
         info = requests.get(url, params=params)
+        if info.status_code:
+            print('Информация о пльзователе получена!')
+        logger.info(f'Получении информации о пользователе <requests.get> - {info.status_code}')
         return str(info.json()['response'][0]['first_name'] +'_'+ info.json()['response'][0]['last_name'])
 
     # Получаем url фото (список словарей с ключами "name", "size", "url")
     def get_url_photos(self, count = 5):
-        logging.debug('Start get_url_photos')
         res = requests.get(self.url, params=self.params)
+        logger.info(f'Получение списка фото <requests.get> - {res.status_code}')
         result = res.json()
         json_dict = {}
         json_list = []
         like_list = [] # список лайков
-        x = 2
         for i in result['response']['items']:
             size = 0
             if len(json_list) <= count-1:
                 for j in i['sizes']:
                     if j['height']  * j['width'] > size:
                         size = j['height']  * j['width']
-                        print(size)
                         img_url = j['url']
                         json_dict['size'] = j['type']
                         json_dict['url'] = img_url
                 like = str(i["likes"]['count'])
                 like_list.append(like)
                 if like_list.count(like) > 1:
-                    json_dict['name'] = f"{like}-{x}.jpg"
-                    x += 1
+                    json_dict['name'] = f"{like}-{i['date']}.jpg"
                 else:
                     json_dict['name'] = f"{like}.jpg"
                 json_list.append(json_dict.copy())
             else:
                 break
+        print(f"найдено {len(json_list)} фотографий!")
         return json_list
 
     # получаем список существующих файлов и папок на YaDisk
     def list_dir(self):
-        logging.debug('Start list_dir')
         url = 'https://cloud-api.yandex.net/v1/disk/resources'
         params = {'path': '/'}
         with open('ya_token.txt') as f:
             self.token = f.read().strip()
         headers = {"Authorization": self.token}
         resp = requests.get(url, params=params, headers=headers)
+        logger.info(f'получаем список существующих файлов и папок на YaDisk <requests.get> - {resp.status_code}')
         ls_dir = []
         for i in resp.json()['_embedded']['items']:
             ls_dir.append(i['name'])
-        logging.debug(f"Получено {len(ls_dir)} файлов!")
+        logging.info(f"Получено {len(ls_dir)} файлов!")
         return ls_dir
 
     # загрузка файлов на YaDisk
     def up_photos(self, dict, dir, ls_dir):
-        logging.debug('Start up_photos')
         # dict - список словарей ключами "name", "size", "url"
         # dir - имя создаваемой папки
         # ls_dir - список существующих файлов и папок на YaDisk
@@ -108,11 +107,13 @@ class VkUser:
                 self.token = f.read().strip()
             headers = {"Authorization": self.token}
             resp = requests.put(url, params=params, headers=headers)
+            logger.info(f'Запрос на создание папки на YaDisk <requests.put> - {resp.status_code}')
             if resp.status_code:
-                logging.debug(f"Каталог {dir} создан!")
+                logger.info(f"каталог {dir} создан!")
+                print(f"каталог {dir} создан!")
         else:
             print(f"Каталог {dir} уже существует!")
-            logging.debug(f"Каталог {dir} уже существует!")
+            logging.info(f"Каталог {dir} уже существует!")
             exit()
         # Загрузка файлов на YaDisk, в созданную папку:
         url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
@@ -122,20 +123,20 @@ class VkUser:
             params = {"path": path}
             headers = {"Authorization": self.token}
             resp = requests.get(url, params=params, headers=headers)
-            logging.debug(resp.status_code)
+            logger.info(f'Получение ссылки для закачки файла <requests.get> - {resp.status_code}')
             url_up = resp.json()['href']
             # Загрузка файла на YaDisk:
             image = requests.get(i['url'])
             up_file = requests.put(url_up, image.content)
-            logging.debug(up_file.status_code)
+            logger.info(f'Загрузка файла на YaDisk <requests.put> - {resp.status_code}')
             if up_file.status_code:
                 print(f"файл '{i['name']}' загружен в папку '{dir}'")
-                logging.debug(f"файл '{i['name']}' загружен в папку '{dir}'")
+                logging.info(f"файл '{i['name']}' загружен в папку '{dir}'")
 
 
 vk_client = VkUser('552934290')
 dir_name = vk_client.user_info()
 # Сохранить указанное количество фотографий(по умолчанию 5):
-img_dict = vk_client.get_url_photos(10)
+img_dict = vk_client.get_url_photos(5)
 ls_dir = vk_client.list_dir()
 vk_client.up_photos(img_dict, dir_name, ls_dir)
